eaa538a1030c0b275f0b3c986f7face34471f277
david sureway <nwachukwudavid100@gmail.com>
2022-04-23 16:35:04 +0100

    just added a comment to tell anyone to create a key and replace the apikey line

--- script.js
+++ script.js
@@ -1,53 +1,54 @@
+//create your apiKey from openWeather and replace in next line
 const apikey = "*********************";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-function url(city){
-    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+function url(city) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 }
 
 //const url = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
 
 async function getWeatherByLocation(city) {
-    const resp = await fetch(url(city), { origin: "cors" });
-    const respData = await resp.json();
+  const resp = await fetch(url(city), { origin: "cors" });
+  const respData = await resp.json();
 
-    console.log(respData);
+  console.log(respData);
 
-    addWeatherToPage(respData);
+  addWeatherToPage(respData);
 }
 
 //this function creates a new div element with a class of weather
 function addWeatherToPage(data) {
-    const temp = KtoC(data.main.temp);
-    const country = data.sys.country;
+  const temp = KtoC(data.main.temp);
+  const country = data.sys.country;
 
-    const weather = document.createElement("div");
-    weather.classList.add("weather");
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
 
-    weather.innerHTML = `
+  weather.innerHTML = `
         <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
         <small>${data.weather[0].main}   <br /> Country: ${country} </small>
     `;
 
-    // cleanup
-    main.innerHTML = "";
+  // cleanup
+  main.innerHTML = "";
 
-    main.appendChild(weather);
+  main.appendChild(weather);
 }
 
 function KtoC(K) {
-    return Math.floor(K - 273.15);
+  return Math.floor(K - 273.15);
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const city = search.value;
+  const city = search.value;
 
-    if (city) {
-        getWeatherByLocation(city);
-    }
+  if (city) {
+    getWeatherByLocation(city);
+  }
 });
